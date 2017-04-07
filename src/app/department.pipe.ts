78627c1cc5b0a45d69415666bfbb69f93b10d572
Caitlin Hines <caitlinhines@me.com>
2017-04-07 11:47:29 -0700

    all functionality complete

--- src/app/department.pipe.ts
+++ src/app/department.pipe.ts
@@ -18,9 +18,38 @@ export class DepartmentPipe implements PipeTransform {
          }
        }
        return output;
-     } else {
-       return input;
-     }
+     } else if (desiredDepartment === "management") {
+       for (var i = 0; i < input.length; i++) {
+         if (input[i].department === "Management") {
+           output.push(input[i]);
+         }
+       }
+       return output;
+     } else if (desiredDepartment === "sound") {
+       for (var i = 0; i < input.length; i++) {
+         if (input[i].department === "Sound") {
+           output.push(input[i]);
+         }
+       }
+       return output;
+     } else if (desiredDepartment === "merchandise") {
+       for (var i = 0; i < input.length; i++) {
+         if (input[i].department === "Merchandise") {
+           output.push(input[i]);
+         }
+       }
+       return output;
+     }  else if (desiredDepartment === "food") {
+        for (var i = 0; i < input.length; i++) {
+          if (input[i].department === "Food") {
+            output.push(input[i]);
+          }
+        }
+        return output;
+      } else {
+
+        return input;
+      }
    }
 
 }
