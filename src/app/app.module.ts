0b5074a8d884cefb15eaa3f1007049f431849c7a
Caitlin Hines <caitlinhines@me.com>
2017-04-07 08:42:33 -0700

    add link to navbar for roster list- works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { routing } from './app.routing';
 import { AboutComponent } from './about/about.component';
+import { RosterComponent } from './roster/roster.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    AboutComponent
+    AboutComponent,
+    RosterComponent
   ],
   imports: [
     BrowserModule,
